ec18dae6633a3abc58c3460b91de3a3362505c14
Bernd Schuller <b.schuller@fz-juelich.de>
2013-09-30 09:14:43 +0000

    use simple json instead of jackson

--- src/main/java/eu/unicore/security/wsutil/client/authn/JsonSecuritySessionPersistence.java
+++ src/main/java/eu/unicore/security/wsutil/client/authn/JsonSecuritySessionPersistence.java
@@ -12,9 +12,8 @@ import java.util.Iterator;
 
 import org.apache.commons.io.FileUtils;
 import org.apache.log4j.Logger;
-
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.node.ObjectNode;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import eu.unicore.util.Log;
 import eu.unicore.util.httpclient.ClientSecuritySession;
@@ -31,11 +30,10 @@ import eu.unicore.util.httpclient.SessionIDProvider;
 public class JsonSecuritySessionPersistence implements SecuritySessionPersistence
 {
 	private static final Logger logger = Log.getLogger(Log.SECURITY, JsonSecuritySessionPersistence.class);
-	private static final ObjectMapper mapper = new ObjectMapper();
-	
+
 	private boolean sessionsEnabled;
 	private File sessionIDFile;
-	
+
 	public JsonSecuritySessionPersistence(boolean sessionsEnabled, String sessionsFile)
 	{
 		this.sessionsEnabled = sessionsEnabled;
@@ -51,57 +49,69 @@ public class JsonSecuritySessionPersistence implements SecuritySessionPersistenc
 	public void storeSessionIDs(SessionIDProvider sessionProvider) throws IOException {
 		if (!sessionsEnabled || sessionProvider == null)
 			return;
-		
+
 		Collection<ClientSecuritySession> sessions = sessionProvider.getAllSessions();
-		ObjectNode sessionsJson = mapper.createObjectNode();
-		
-		for(ClientSecuritySession entry: sessions){
-			String serverID=entry.getScope();
-			ObjectNode info = mapper.createObjectNode();
-			info.put("sessionID", entry.getSessionId());
-			info.put("hash", entry.getSessionHash());
-			info.put("expiry", entry.getExpiryTS());
-			sessionsJson.put(serverID,info);
-		}
-		if (sessions.size()>0) {
-			if (sessionIDFile != null) {
-				FileWriter writer=new FileWriter(sessionIDFile);
-				mapper.writeValue(writer, sessionsJson);
-				writer.close();
-				logger.debug("Stored <"+sessions.size()+"> security session ID(s) to <"+
-						sessionIDFile.getAbsolutePath());
+		JSONObject sessionsJson = new JSONObject();
+		try{
+			for(ClientSecuritySession entry: sessions){
+				String serverID=entry.getScope();
+				JSONObject info = new JSONObject();
+				info.put("sessionID", entry.getSessionId());
+				info.put("hash", entry.getSessionHash());
+				info.put("expiry", entry.getExpiryTS());
+				sessionsJson.put(serverID,info);
 			}
+			if (sessions.size()>0) {
+				if (sessionIDFile != null) {
+					FileWriter writer=new FileWriter(sessionIDFile);
+					try{
+						sessionsJson.write(writer);
+					}finally{
+						writer.close();
+					}
+					logger.debug("Stored <"+sessions.size()+"> security session ID(s) to <"+
+							sessionIDFile.getAbsolutePath());
+				}
+			}
+		}catch(JSONException e){
+			throw new IOException(e);
 		}
 	}
 
 	@Override
+	@SuppressWarnings("unchecked")
 	public void readSessionIDs(SessionIDProvider sessionProvider) throws IOException 
 	{
 		if (!sessionsEnabled || sessionProvider == null)
 			return;
-		
+
 		if (sessionIDFile != null && sessionIDFile.exists()) {
-			ObjectNode sessionsJSON = (ObjectNode) mapper.readTree(FileUtils.readFileToString(sessionIDFile));
-			Iterator<String>serverIDs = sessionsJSON.fieldNames();
-			while(serverIDs.hasNext()){
-				String serverID=serverIDs.next();
-				ObjectNode info=(ObjectNode) sessionsJSON.get(serverID);
-				String hash=info.get("hash").asText();
-				String sessionID=info.get("sessionID").asText();
-				Long lifetime = info.get("expiry").asLong();
-				if(lifetime!=null && lifetime<System.currentTimeMillis()){
-					if(logger.isDebugEnabled())logger.debug("Session for "+serverID+" expired");
-					continue;
+			try{
+				JSONObject sessionsJSON = new JSONObject(FileUtils.readFileToString(sessionIDFile));
+				Iterator<String>serverIDs = sessionsJSON.keys();
+				while(serverIDs.hasNext()){
+					String serverID=serverIDs.next();
+					JSONObject info=sessionsJSON.getJSONObject(serverID);
+					String hash=info.getString("hash");
+					String sessionID=info.getString("sessionID");
+					Long lifetime = info.optLong("expiry",-1);
+					if(lifetime!=null && lifetime<System.currentTimeMillis()){
+						if(logger.isDebugEnabled())logger.debug("Session for "+serverID+" expired");
+						continue;
+					}
+					if (logger.isDebugEnabled())
+						logger.debug("Re-adding session for " + serverID + " id " + sessionID);
+					ClientSecuritySession s = new ClientSecuritySession(sessionID, lifetime, 
+							hash, serverID);
+					sessionProvider.addSession(s);
 				}
-				if (logger.isDebugEnabled())
-					logger.debug("Re-adding session for " + serverID + " id " + sessionID);
-				ClientSecuritySession s = new ClientSecuritySession(sessionID, lifetime, 
-						hash, serverID);
-				sessionProvider.addSession(s);
+			}catch(JSONException e){
+				// ignore
 			}
-			
-			logger.debug("Loaded <"+sessionProvider.getAllSessions().size() +
-					"> security session ID(s) from <" + sessionIDFile.getAbsolutePath());
 		}
+
+		logger.debug("Loaded <"+sessionProvider.getAllSessions().size() +
+				"> security session ID(s) from <" + sessionIDFile.getAbsolutePath());
 	}
 }
+
